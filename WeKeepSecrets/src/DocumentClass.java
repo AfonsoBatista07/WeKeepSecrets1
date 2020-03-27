e3e3c3da839c02322f5a1c8e4cecc3073a808977
JaJoge <joaojorg4@gmail.com>
2020-03-27 15:45:40 +0000

    Document Class fully implemented
    SecuritySystem Class and Interface squeleton

---
+++ WeKeepSecrets/src/DocumentClass.java
@@ -0,0 +1,44 @@
+
+public class DocumentClass implements Document{
+	private String manager, name, level, description;
+	private int current;
+	private String[] lastRead;
+	
+	private static final int MAX_NAMES = 10;
+	
+	public DocumentClass(String manager, String name, String level, String description) {
+		this.manager = manager;
+		this.name = name;
+		this.level = level;
+		this.description = description;
+		
+		current = 0;
+		
+		lastRead = new String[MAX_NAMES];
+	}
+	
+	public void read(String userId) {
+		if(current == MAX_NAMES) {
+			current = 0;
+		}
+		lastRead[current++] = userId;
+	}
+	
+	public String getLevel() {
+		return level;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getManager() {
+		return manager;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+}
