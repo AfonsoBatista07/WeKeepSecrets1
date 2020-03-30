130eb68433616fb767580c0dd13bfae2a150d081
JaJoge <joaojorg4@gmail.com>
2020-03-30 16:23:47 +0100

    Finished ClassifiedDoc

--- WeKeepSecrets/src/securitySystem/ClassifiedDocumentClass.java
+++ WeKeepSecrets/src/securitySystem/ClassifiedDocumentClass.java
@@ -3,26 +3,80 @@ package securitySystem;
 public class ClassifiedDocumentClass extends DocumentClass{
 	private String level;
 	
-	private String[] grants;
+	private String[] grants, whoAccessed, action, grantsRevoked;
 	
-	private int numGrants;
+	private int currentAccess, currentGrant, currentRevokedGrant;
 	
 	private static final int MAX_GRANTS = 100;
 
 	public ClassifiedDocumentClass(String manager, String name, String level, String description) {
 		super(manager, name, description);
 		this.level = level;
-		
-		numGrants = 0;
+		currentAccess = 0;
+		currentGrant = 0;
+		currentRevokedGrant = 0;
+		whoAccessed = new String[MAX_GRANTS];
+		action = new String[MAX_GRANTS];
 		grants = new String[MAX_GRANTS];
+		grantsRevoked = new String[MAX_GRANTS];
 	}
 	
-	public void write(String description) {
+	public void read(String userId) {
+		whoAccessed[currentAccess] = userId;
+		action[currentAccess]= "read";
+		currentAccess++;
+	}
+	
+	public String getLevel() {
+		return level;
+	}
+	
+	public int getNumGrants() {
+		return grants.length;
+	}
+	
+	public void addGrant(String userId) {
+		if(currentGrant == grants.length) {
+			resize(grants, currentGrant);
+		}
+		grants[currentGrant++] = userId;
+	}
+	
+	public void revokeGrant(String userId) {
+		for( int i = findPos(userId); i < currentGrant-1; i++ ) {
+			grants[i]= grants[i+1];
+		}
+		if(currentRevokedGrant == grantsRevoked.length) {
+			resize(grantsRevoked, currentRevokedGrant);
+		}
+		grantsRevoked[currentRevokedGrant++] = userId;
+	}
+	
+	public void write(String description, String userId) {
 		super.description = description;
+		whoAccessed[currentAccess] = userId;
+		action[currentAccess]= "write";
+		currentAccess++;
 	}
 	
-	// falta lista de names e se são write ou read
-	//falta tb implementar o metodo de dar grants
+	private int findPos(String userId) {
+		int i = 0;
+		while ((i < currentGrant)) {
+			if (grants[i].contentEquals(userId)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+	
+	private void resize(String[] list, int current) {
+		String[] sl = new String[10 * list.length];
+		for (int i = 0; i < current; i++)
+			sl[i] = list[i];
+		list = sl;
+
+	}
 	
 
 }
