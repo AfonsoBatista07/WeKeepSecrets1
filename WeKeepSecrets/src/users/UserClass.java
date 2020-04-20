370825c70ec959706a23c7124c8f37a82e8bf90d
JaJoge <joaojorg4@gmail.com>
2020-04-20 11:32:28 +0100

    Burrice

--- WeKeepSecrets/src/users/UserClass.java
+++ WeKeepSecrets/src/users/UserClass.java
@@ -18,7 +18,7 @@ public class UserClass implements User {
 	private int current;
 	
 	private static final int MAX_DOCS = 500, GROW_FACTOR = 2;
-	protected static final String CLERK = "clerk" ;
+	protected static final String OFFICER = "officer", CLERK = "clerk" ;
 	
 	public UserClass(String id, String level, String kind) {
 		this.id = id;
